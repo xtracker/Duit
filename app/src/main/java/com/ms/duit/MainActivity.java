b7e02c17daeeb3343e92899f4f85aca25f338c59
Jarvis <jarvis-xera@hotmail.com>
2015-04-06 23:24:15 +0800

    Add navigation drawer

--- app/src/main/java/com/ms/duit/MainActivity.java
+++ app/src/main/java/com/ms/duit/MainActivity.java
@@ -1,29 +1,65 @@
 package com.ms.duit;
 
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+import com.ms.duit.slidetabs.SlidingTabLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private ViewPager mViewPager;
+
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private RadioGroup mDrawerRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mViewPager = (ViewPager)findViewById(R.id.pager_main);
-        mViewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager(), this));
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.navigation_drawer);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.hello_world, R.string.hello_world) {
+
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle("Duit");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("Opened");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+
+        mDrawerRadioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+
+        selectItem(0);
     }
 
 
@@ -39,6 +75,10 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -47,6 +87,57 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void selectItem(int position) {
+
+        Fragment fragment = ContentFragment.newInstance(position);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.content_frame, fragment);
+        ft.commit();
+
+        // update selected item title, then close the drawer
+        mDrawerLayout.closeDrawer(mDrawerRadioGroup);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+         mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public static class ContentFragment extends Fragment {
+
+        private ViewPager mViewPager;
+        private SlidingTabLayout mSlidingTabLayout;
+        public ContentFragment() {}
+
+        public static ContentFragment newInstance(int position) {
+            return new ContentFragment();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_content, container, false);
+
+            mViewPager = (ViewPager)rootView.findViewById(R.id.pager_main);
+            mViewPager.setAdapter(new MainPagerAdapter(getActivity().getSupportFragmentManager(), getActivity()));
+            mSlidingTabLayout = (SlidingTabLayout) rootView.findViewById(R.id.sliding_tabs);
+            mSlidingTabLayout.setViewPager(mViewPager);
+            mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.indicator_selected));
+            return rootView;
+        }
     }
 
     /**
